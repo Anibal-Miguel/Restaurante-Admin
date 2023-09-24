98bf99c656857eefee3a19ff4fd5f56167cdb2ff
Segovia Anibal Miguel <segoviaanibal@gmail.com>
2023-09-24 19:57:20 -0300

    commit 2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,6 +7,7 @@ const login = () => {
 
         <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div className="modal-dialog">
+              
                 <div className="modal-content">
                     <div className="modal-header">
                         <h1 className="modal-title fs-5" id="exampleModalLabel">New message</h1>
